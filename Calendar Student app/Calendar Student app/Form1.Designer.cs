6e4231f6d6d9f1337ed53657d5a6208894be97bc
DavidM-32 <david.marcell@diak.szbi-pg.hu>
2026-03-29 20:03:43 +0200

    asd

--- Calendar Student app/Calendar Student app/Form1.Designer.cs
+++ Calendar Student app/Calendar Student app/Form1.Designer.cs
@@ -184,7 +184,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(694, 450);
+            this.button2.Location = new System.Drawing.Point(706, 450);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 13;
@@ -193,7 +193,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(613, 450);
+            this.button1.Location = new System.Drawing.Point(625, 450);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 12;
